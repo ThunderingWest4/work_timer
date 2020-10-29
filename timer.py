bb1b1af3bad75e2a0c4a7d7f4ee5829a6c4ed291
thunderingwest4 <aaravgupta04@gmail.com>
2020-10-28 23:45:08 -0400

    initial commit, base functionalities

---
+++ timer.py
@@ -0,0 +1,26 @@
+import time
+import sys
+from termcolor import colored
+from tqdm import tqdm
+import os
+
+# Formatting of command: 
+# python timer.py n_cycles len_work len_break
+sysargs = [str(x) for x in sys.argv]
+#first arg is gonna be timer.py
+n_cycles = int(sysargs[1])
+len_work = int(sysargs[2])
+len_break = int(sysargs[3])
+
+# time.sleep(t) works with t *seconds*
+
+for n in range(n_cycles):
+    if n_cycles>1: print(colored(f"Cycle {n+1} of {n_cycles} cycles with {len_work} minute(s) working and {len_break} minute(s) on break per cycle", 'blue')) 
+    
+    print(colored(f"Work Time! Try to focus on your work for {len_work} minute(s)", 'green'))
+    for i in tqdm(range(4*len_work), desc="Working time"):
+        time.sleep(15) # sleep 15 seconds
+    print(colored(f"Break Time! {len_break} minute(s) off", 'blue'))
+    for i in tqdm(range(4*len_break), desc="Break time"):
+        time.sleep(15)
+
